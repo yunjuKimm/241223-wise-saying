60273683dc92d2984d1489f75dc2b9310f37ac9f
yunjuKimm <yunju.tech@gmail.com>
2024-12-24 01:46:28 +0900

    step4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,6 +15,8 @@ class App {
         Scanner scanner = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
 
+        int lastNo = 1;
+
         while (true) {
             System.out.print("명령) ");
             String command = scanner.nextLine();
@@ -23,13 +25,16 @@ class App {
                 System.out.println("명언 앱을 종료합니다.");
                 break;
             } else if (command.equals("등록")) {
+
+
                 System.out.print("명언 : ");
                 scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨. 그래야 넘어감.
 
                 System.out.print("작가 : ");
                 scanner.nextLine();
 
-                System.out.println("1번 명언이 등록되었습니다.");
+                System.out.println(lastNo + "번 명언이 등록되었습니다.");
+                lastNo++;
             }
 
 
