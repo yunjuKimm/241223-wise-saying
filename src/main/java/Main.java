83aaaa13b58e611911870c43203474cc8772a233
yunjuKimm <yunju.tech@gmail.com>
2024-12-23 17:37:13 +0900

    work3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         App app = new App();
@@ -9,7 +11,18 @@ public class Main {
 
 class App {
     public void run() {
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("== 명언 앱 ==");
+
+        System.out.println("명령) 등록");
+        System.out.print("명언 : ");
+        scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨. 그래야 넘어감.
+        System.out.print("작가 : ");
+        scanner.nextLine();
+        System.out.println("1번 명언이 등록되었습니다.");
+
         System.out.println("명령) 종료");
     }
 }
