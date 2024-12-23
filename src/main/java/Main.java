686fca3adcc9483a65b8bdfda6aab5e56018c6c3
yunjuKimm <yunju.tech@gmail.com>
2024-12-23 17:20:13 +0900

    work2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        App app = new App();
+        app.run();
 
+    }
+}
+
+
+class App {
+    public void run() {
         System.out.println("== 명언 앱 ==");
         System.out.println("명령) 종료");
-
     }
 }
