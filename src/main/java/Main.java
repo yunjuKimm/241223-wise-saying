63253ff824903bdb5004ce687c2a4103f6930fae
yunjuKimm <yunju.tech@gmail.com>
2024-12-23 17:05:15 +0900

    work1 - print

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println("== 명언 앱 ==");
+        System.out.println("명령) 종료");
+
     }
 }
