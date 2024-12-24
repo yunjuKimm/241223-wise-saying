298f368dc272a0faae30012734ed8f62d1d37ed1
yunjuKimm <yunju.tech@gmail.com>
2024-12-24 12:46:16 +0900

    Logic as a method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
 
 public class App {
-
     // 클래스에서 함수들이 공유해야 하는 변수 -> 인스턴스 변수
     private int lastId = 0; // 명언 번호
     private int wiseSayingSize = 0;
     private final WiseSaying[] wiseSayingList = new WiseSaying[3]; // 명언들을 저장할 리스트
 
-    public void run() {
+    Scanner scanner = new Scanner(System.in);
 
-        Scanner scanner = new Scanner(System.in);
+    public void run() {
 
         // 테스트 명언 데이터1
         add("꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "월트 디즈니");
@@ -25,31 +24,15 @@ public class App {
                 break;
 
             } else if (command.equals("등록")) {
-
-                System.out.print("명언 : ");
-                String content = scanner.nextLine(); // 명언 입력 -> 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨. 그래야 넘어감.
-
-                System.out.print("작가 : ");
-                String author = scanner.nextLine(); // 작가 입력
-
-                add(content, author);  // 1. 함수로 분리 -> 코드가 줄어든다. 가독성이 올라간다. 2. 재활용성(중복 제거)
-
-                System.out.println("%d번 명언이 등록되었습니다. %s".formatted(lastId));
+                writeWiseSaying();
 
             } else if (command.equals("목록")) {
+                printWiseSayingList();
 
-                System.out.println("번호 / 작가 / 명언");
-                System.out.println("----------------------");
-
-                //등록된 명언 목록 출력
-                for (int i = 0; i < wiseSayingSize; i++) {
-                    WiseSaying wiseSaying = wiseSayingList[i];
-                    System.out.println("%d / %s / %s".formatted(wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent()));
-                }
+            } else {
+                System.out.println("알 수 없는 명령어입니다.");
 
             }
-
-
         }
     }
 
@@ -59,4 +42,25 @@ public class App {
         wiseSayingList[wiseSayingSize++] = wiseSaying;
     }
 
+    private void writeWiseSaying() {
+        System.out.print("명언 : ");
+        String content = scanner.nextLine(); // 명언 입력 -> 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨. 그래야 넘어감.
+
+        System.out.print("작가 : ");
+        String author = scanner.nextLine(); // 작가 입력
+
+        add(content, author);  // 1. 함수로 분리 -> 코드가 줄어든다. 가독성이 올라간다. 2. 재활용성(중복 제거)
+        System.out.println("%d번 명언이 등록되었습니다. %s".formatted(lastId));
+    }
+
+    private void printWiseSayingList() {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("----------------------");
+
+        for (int i = 0; i < wiseSayingSize; i++) {
+            WiseSaying wiseSaying = wiseSayingList[i];
+            System.out.println("%d / %s / %s".formatted(wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent()));
+        }
+
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,3 @@ public class Main {
         app.run();
     }
 }
-
-
